b3103bdb555f1193d90f23dd46f4d1ee17699e43
Parisa Khateri <pkhateri@miacag-mit-220.medradio.unibas.ch>
2023-06-22 13:48:32 +0200

    fix bug infere_visualize_oct_seg.py

--- datasets/infere_visualize_oct_seg.py
+++ datasets/infere_visualize_oct_seg.py
@@ -43,9 +43,8 @@ def get_oct_dicts(img_dir, annot_dir):
         record["width"] = img_annot['imageWidth']
         
         objs = []
-        #print(img_annot["shapes"])
         for box in img_annot["shapes"]:
-            print(box['points'])
+            print('\tbounding box:', box['points'])
             p1 = box['points'][0]
             p2 = box['points'][1]
             obj = {
@@ -55,20 +54,21 @@ def get_oct_dicts(img_dir, annot_dir):
             }
             objs.append(obj)
         record["annotations"] = objs
-        print(objs)
-        #if x_box[0]==x_box[1]==0:
-        #    record["annotations"] = []
         dataset_dicts.append(record)
     return dataset_dicts
 
-def infer(img_dir, annot_dir, output_dir, model_path, weight_dir):
+def infer(img_dir, annot_dir, output_dir, model_path, weights_dir):
     '''
-    load the input files
+    1. register the get_oct_dicts function
+        note that here the function is not called
+    2. load dataset into a dictionary
     '''
     for d in ["test"]:
-        DatasetCatalog.register("oct_" + d, lambda d=d: get_oct_dicts(img_dir, annot_dir))
+        dataset_dicts = get_oct_dicts(img_dir+"test", annot_dir+"test")
+        DatasetCatalog.register("oct_" + d, lambda d=d: get_oct_dicts(img_dir+"test", annot_dir+"test")) # register your function
         MetadataCatalog.get("oct_" + d).set(thing_classes=["damaged"])
-    oct_metadata = MetadataCatalog.get("oct_train")
+    oct_metadata = MetadataCatalog.get("oct_test")
+    dataset_dicts = get_oct_dicts(img_dir+"test", annot_dir+"test")
 
     '''
     load the model
@@ -76,7 +76,7 @@ def infer(img_dir, annot_dir, output_dir, model_path, weight_dir):
     from detectron2.modeling import build_model
     from detectron2.checkpoint import DetectionCheckpointer
     cfg = get_cfg()
-    cfg.merge_from_file(model_zoo.get_config_file(model_dir))
+    cfg.merge_from_file(model_zoo.get_config_file(model_path))
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # has five classes, one for each layer. (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
     cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = False # to use those data with empty annotation
     cfg.INPUT.FORMAT = "L" # input images are black and white
@@ -94,7 +94,6 @@ def infer(img_dir, annot_dir, output_dir, model_path, weight_dir):
     predictor = DefaultPredictor(cfg)
 
     from detectron2.utils.visualizer import ColorMode
-    dataset_dicts = get_oct_dicts(img_dir, annot_dir)
     for d in random.sample(dataset_dicts, 5):    
         im = cv2.imread(d["file_name"])
         outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
@@ -124,7 +123,7 @@ def infer(img_dir, annot_dir, output_dir, model_path, weight_dir):
         
     from detectron2.evaluation import COCOEvaluator, inference_on_dataset
     from detectron2.data import build_detection_test_loader
-    evaluator = COCOEvaluator("oct_test", output_dir)
+    evaluator = COCOEvaluator("oct_test", output_dir=output_dir)
     test_loader = build_detection_test_loader(cfg, "oct_test")
     print(inference_on_dataset(predictor.model, test_loader, evaluator))
 
@@ -137,13 +136,13 @@ def main(argv):
     img_dir = "/projects/parisa/data/test_boxal/faster_rcnn/"
     annot_dir = "/projects/parisa/data/test_boxal/faster_rcnn/"
     output_dir = "./output/"
-    model_dir = "./COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
-    weight_dir = "./weights/exp1/uncertainty/"
+    model_path = "./COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
+    weights_dir = "./weights/exp1/uncertainty/"
     
-    opts, args = getopt.getopt(argv,"hi:a:o:m:w:",["img_dir=","annot_dir=","output_dir=", "path_to_model=", "weight_dir="])
+    opts, args = getopt.getopt(argv,"hi:a:o:m:w:",["img_dir=","annot_dir=","output_dir=", "path_to_model=", "weights_dir="])
     for opt, arg in opts:
         if opt == '-h':
-            print('python infere_visualize_oct_seg.py -i <image_dir> -a <annotation_dir> -o <output_dir> -m <path_to_model> -w <weight_dir>')
+            print('python infere_visualize_oct_seg.py -i <image_dir> -a <annotation_dir> -o <output_dir> -m <path_to_model> -w <weights_dir>')
             sys.exit()
         elif opt in ("-i", "--img_dir"):
             img_dir = arg
@@ -153,21 +152,21 @@ def main(argv):
             output_dir = arg
         elif opt in ("-m", "--path_to_model"):
             model_path = arg
-        elif opt in ("-w", "--weight_dir"):
-            weight_dir = arg
+        elif opt in ("-w", "--weights_dir"):
+            weights_dir = arg
     if len(argv) < 6:
-        print("WARNING: default inputs will be used: {}, {}, {}, {}, {}".format(img_dir, annot_dir, output_dir, model_path, weight_dir))
+        print("WARNING: default inputs will be used: {}, {}, {}, {}, {}".format(img_dir, annot_dir, output_dir, model_path, weights_dir))
 
     if not os.path.exists(img_dir):
         sys.exit("ERROR: {} does not exist!".format(img_dir))
     if not os.path.exists(annot_dir):
         sys.exit("ERROR: {} does not exist!".format(annot_dir))
-    if not os.path.exists(weight_dir):
-        sys.exit("ERROR: {} does not exist!".format(weight_dir))
+    if not os.path.exists(weights_dir):
+        sys.exit("ERROR: {} does not exist!".format(weights_dir))
     if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         
-    infer(img_dir, annot_dir, output_dir, model_path, weight_dir)
+    infer(img_dir, annot_dir, output_dir, model_path, weights_dir)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
