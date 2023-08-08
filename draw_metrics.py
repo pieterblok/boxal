75f685d5dfded4579c8983815588df2b5fd56ea9
Parisa Khateri <pkhateri@miacag-mit-608.medradio.unibas.ch>
2023-08-08 14:47:11 +0200

    modify draw_metrics:
    - draw AP plots too
    
    - define y limit as input
    
    - plot file name is given as prefix not complete name

--- draw_metrics.py
+++ draw_metrics.py
@@ -8,21 +8,25 @@ def main(argv):
     input agruments
     '''
     metrics_filename = ""
-    output_plot_filename = ""
+    output_plot_filename_prefix = ""
     plot_all = False
+    y_limit = 0
 
-    opts, args = getopt.getopt(argv,"hi:o:n",["input_filename=","output_plot_filename="])
+    opts, args = getopt.getopt(argv,"hi:o:y:p",["input_filename=","output_plot_filename_prefix=", "y_limit="])
     for opt, arg in opts:
         if opt == '-h':
             print('python draw_metrics.py -i <input_filename> -o <output_plot_filename> -p\n \
                 -i (--input_filename): input_filename is the path to the metrics.json file\n \
                 -o (--output_plot_filename): output_plot_filename is the path to the name of png file to save the plots\n \
-                -p (--plot all): plot all other losses other than the total loss. (Default=False)')
+                -p (--plot_all): plot all other losses other than the total loss. (Default=False)\n \
+                -y (--y_limit): limit on the maximum of y axis for the loss value')
             sys.exit()
         elif opt in ("-i", "--input_filename"):
             metrics_filename = arg
-        elif opt in ("-o", "--output_plot_filename"):
-            output_plot_filename = arg
+        elif opt in ("-o", "--output_plot_filename_prefix"):
+            output_plot_filename_prefix = arg
+        elif opt in ("-y", "--y_limit"):
+            y_limit = float(arg)
         elif opt in ("-p", "--plot_all"):
             plot_all = True
 
@@ -31,8 +35,9 @@ def main(argv):
         sys.exit('ERROR: input arguments not provided!\n \
             python draw_metrics.py -i <input_filename> -o <output_plot_filename> -p\n \
                 -i (--input_filename): input_filename is the path to the metrics.json file\n \
-                -o (--output_plot_filename): output_plot_filename is the path to the name of png file to save the plots\n \
-                -p (--plot all) - optional: plot all other losses other than the total loss. (Default=False)')
+                -o (--output_plot_filename_prefix): is the path + prefix to the name of png file to save the plots\n \
+                -p (--plot all) - optional: plot all other losses other than the total loss. (Default=False)\n \
+                -y (--y_limit): limit on the maximum of y axis for the loss value')
 
     if not os.path.exists(metrics_filename):
         sys.exit("ERROR: input file does not exist! {}".format(metrics_filename))
@@ -43,6 +48,7 @@ def main(argv):
     '''
     train_it = [] # training iteration number
     val_it = []
+    ap_it = []
     loss_tot = []
     loss_rpn_cls = []
     loss_rpn_loc = []
@@ -51,13 +57,16 @@ def main(argv):
     accu = []
     loss_sum = []
     loss_val = []
+    ap = []
+    ap50 = []
+    ap75 = []
     with open(metrics_filename) as f:
         for json_obj in f:
             dict = json.loads(json_obj)
             if "data_time" in dict:
                 train_it.append(dict['iteration'])
                 loss_tot.append(dict['total_loss'])
-                accu.append(dict['fast_rcnn/cls_accuracy'])
+#                accu.append(dict['fast_rcnn/cls_accuracy'])
                 loss_rpn_cls.append(dict['loss_rpn_cls'])
                 loss_rpn_loc.append(dict['loss_rpn_loc'])
                 loss_box_reg.append(dict['loss_box_reg'])
@@ -65,9 +74,14 @@ def main(argv):
                 if 'validation_loss' in dict:
                     loss_val.append(dict['validation_loss'])
                     val_it.append(dict['iteration'])
+                if "bbox/AP" in dict:
+                    ap.append(dict["bbox/AP"])
+                    ap50.append(dict["bbox/AP50"])
+                    ap75.append(dict["bbox/AP75"])
+                    ap_it.append(dict['iteration'])
                 s = dict['loss_rpn_cls'] + dict['loss_rpn_loc'] + dict['loss_box_reg'] + dict['loss_cls']
                 loss_sum.append(s)
-
+    
     accumulate_it = 0
     for i, it in enumerate(train_it):
         if i+1 <len(train_it):
@@ -82,33 +96,28 @@ def main(argv):
                 accumulate_it += val_it[i]+1
         val_it[i] = it + accumulate_it
 
+    accumulate_it = 0
+    for i, it in enumerate(ap_it):
+        if i+1 <len(ap_it):
+            if ap_it[i] > ap_it[i+1]:
+                accumulate_it += ap_it[i]+1
+        ap_it[i] = it + accumulate_it
 
     '''
     - sort arrays to get nice plots
     - assign repeated elements to zero, just to ignore them.
     - sort them once more to move zeroed elements to the begining
     '''
-    train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum = zip(*sorted(zip(train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum)))
+#    train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum = zip(*sorted(zip(train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum)))
+    train_it, loss_tot, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum = zip(*sorted(zip(train_it, loss_tot, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum)))
     if len(val_it)>0:
         val_it, loss_val = zip(*sorted(zip(val_it, loss_val)))
+    if len(ap_it)>0:
+        ap_it, ap, ap50, ap75 = zip(*sorted(zip(ap_it, ap, ap50, ap75)))
 
-
-#    temp=-1
-#    for idx,it in enumerate(train_it):
-#        if it==temp:
-#            train_it[idx-1]=0
-#            loss_tot[idx-1]=0
-#            loss_rpn_cls[idx-1]=0
-#            loss_rpn_loc[idx-1]=0
-#            loss_box_reg[idx-1]=0
-#            loss_cls[idx-1]=0
-#            accu[idx-1]=0
-#        else:
-#            temp = it
-#    train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum = [list(t) for t in zip(*sorted(zip(train_it, loss_tot, accu, loss_rpn_cls, loss_rpn_loc, loss_box_reg, loss_cls, loss_sum)))]
 
     '''
-    draw plots
+    draw loss plots
     '''
     plt.figure(figsize=(12, 7))
     if not plot_all:
@@ -124,13 +133,33 @@ def main(argv):
     #plt.plot(train_it, accu, marker = 'o', markersize=3, linestyle='none', fillstyle='none')
 
     plt.xlim([0,max(train_it)+1])
-    plt.ylim([0,0.4])
+    if y_limit>0: plt.ylim([0,y_limit])
     plt.grid()
     plt.legend()
     plt.xlabel("Iteration")
     plt.ylabel("Loss values")
     plt.tight_layout()
-    plt.savefig(output_plot_filename)
+    plt.savefig(output_plot_filename_prefix+"_loss.png")
+    plt.close()
+    
+    '''
+    draw AP plots
+    '''
+    plt.figure(figsize=(12, 7))
+    if ap!=[]:
+        plt.plot(ap_it, ap, label = 'mAP', linestyle='-', color='tab:blue')
+        plt.plot(ap_it, ap50, label = 'AP50', linestyle='-', color='tab:red')
+        plt.plot(ap_it, ap75, label = 'AP75', linestyle='-', color='tab:green')
 
+    plt.xlim([0,max(train_it)+1])
+    #if y_limit>0: plt.ylim([0,y_limit])
+    plt.grid()
+    plt.legend()
+    plt.xlabel("Iteration")
+    plt.ylabel("AP values")
+    plt.tight_layout()
+    plt.savefig(output_plot_filename_prefix+"_ap.png")
+    plt.close()
+    
 if __name__ == "__main__":
     main(sys.argv[1:])
